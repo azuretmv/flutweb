298d3c19379af181b1c75a7b00d31b1814956d2c
azuretmv <azure@turkiyemaarif.org>
2020-12-28 20:47:07 +0300

    second day

---
+++ lib/constants/strings.dart
@@ -0,0 +1,23 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'Flutter';
+  static const String me = ' Web';
+  static const String portfoli = 'Flutter Web &';
+  static const String o = ' Azure';
+  static const String headline =
+      'I\'m Eyup Turkay, Fullstack Software Engineer from Turkiye!';
+  static const String summary =
+      'Website by Flutter on Azure Static Wep Apps. Flutter is Awesome, Flutter web is even more awesome! Azure static web application service is also a great tool announce at MSBuild 2020! We can use both of them to host static website! Here is a demo! Reach me out for more info!';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved =
+      '© 2020 Turkiye Maarif Foundation by Eyup Turkay. ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'Azure';
+  static const String menu_contact = 'Flutter';
+  static const String menu_majid = 'Eyup Turkay';
+}
